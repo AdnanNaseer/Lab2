87ddbb5cbf70238e2bc00dc5202147156fb5848b
AdnanNaseer <t8naad00@students.oamk.fi>
2020-05-03 19:55:49 +0300

    inital commit

---
+++ app/src/main/java/com/example/adnan_lab2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.adnan_lab2;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        final int[] counter = {0};
+        final Button gameButton = new Button(this);
+
+        setContentView(gameButton);
+
+        super.onCreate(savedInstanceState);
+
+        gameButton.setText("Click Me !");
+
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter[0]++;
+                gameButton.setText("You clicked me " + String.valueOf(counter[0]) + " times");
+            }
+        });
+
+    }
+}
